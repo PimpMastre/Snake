e4e6194eef904e798db83bf2dd975a10277297b9
PimpMastre <pmpmstrr@hotmail.com>
2017-02-02 00:37:17 +0200

    Made the main part of the game
    -game panel
    -timer for game speed
    -placeholder buttons

--- Snake/Snake/Form1.Designer.cs
+++ Snake/Snake/Form1.Designer.cs
@@ -28,22 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.buttonHighScores = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timerGameSpeed = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.Location = new System.Drawing.Point(224, 113);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(199, 33);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "SNAKE TITLE";
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(278, 214);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(75, 23);
+            this.buttonPlay.TabIndex = 1;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // buttonHighScores
+            // 
+            this.buttonHighScores.Location = new System.Drawing.Point(278, 282);
+            this.buttonHighScores.Name = "buttonHighScores";
+            this.buttonHighScores.Size = new System.Drawing.Size(75, 23);
+            this.buttonHighScores.TabIndex = 2;
+            this.buttonHighScores.Text = "High scores";
+            this.buttonHighScores.UseVisualStyleBackColor = true;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(278, 345);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 3;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.buttonExit);
+            this.panel1.Controls.Add(this.labelTitle);
+            this.panel1.Controls.Add(this.buttonHighScores);
+            this.panel1.Controls.Add(this.buttonPlay);
+            this.panel1.Location = new System.Drawing.Point(-1, -1);
+            this.panel1.MaximumSize = new System.Drawing.Size(647, 656);
+            this.panel1.MinimumSize = new System.Drawing.Size(647, 656);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(647, 656);
+            this.panel1.TabIndex = 4;
+            // 
+            // timerGameSpeed
+            // 
+            this.timerGameSpeed.Interval = 250;
+            this.timerGameSpeed.Tick += new System.EventHandler(this.timerGameSpeed_Tick);
             // 
             // Snake
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
-            this.MaximumSize = new System.Drawing.Size(800, 600);
-            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.ClientSize = new System.Drawing.Size(639, 638);
+            this.Controls.Add(this.panel1);
+            this.MaximumSize = new System.Drawing.Size(655, 677);
+            this.MinimumSize = new System.Drawing.Size(655, 677);
             this.Name = "Snake";
             this.Text = "Snake";
+            this.Load += new System.EventHandler(this.Snake_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Button buttonHighScores;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timerGameSpeed;
     }
 }
 
--- Snake/Snake/Form1.cs
+++ Snake/Snake/Form1.cs
@@ -12,9 +12,87 @@ namespace Snake
 {
     public partial class Snake : Form
     {
+        struct Tail
+        {
+            public int xPos, yPos, TPDirection; // TPDirection - 1 - sus, 2 - dreapta, 3 - jos, 4 - stanga
+            public bool isTurningPoint;
+        }
+
+        int direction = 0; // 0 inseamna nimic, 1- sus, 2- dreapta, 3- jos, 4- stanga
+        int pictureBoxSize;
+        public static PictureBox[,] PB;
+        public static int[,] gameBoard;
+        Tail[] tail;
+        Writing writing = new Writing(); //AESTHETIC, ONLY IMPLEMENT AT THE END
+        
         public Snake()
         {
             InitializeComponent();
+        }
+
+        private void Snake_Load(object sender, EventArgs e)
+        {
+            PB = new PictureBox[33, 33];
+            gameBoard = new int[33, 33];
+            tail = new Tail[201];
+            pictureBoxSize = panel1.Size.Height / 32;  // 640x640 e marimea formului, se imparte la 32
+            for (int i = 1; i <= 32; ++i)
+                for(int j = 1; j <= 32; ++j)
+                {
+                    PB[i, j] = new PictureBox();
+                    PB[i, j].Height = pictureBoxSize;
+                    PB[i, j].Width = pictureBoxSize;
+                    PB[i, j].Top = (pictureBoxSize) * (i - 1);
+                    PB[i, j].Left = (pictureBoxSize) * (j - 1);
+                    PB[i, j].BackColor = Color.Black;
+                    PB[i, j].Parent = panel1;
+                }
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            labelTitle.Visible = false; // ascundem butoanele/labelurile
+            buttonPlay.Visible = false;
+            buttonHighScores.Visible = false;
+            buttonExit.Visible = false;
+
+            PB[32 / 2, 32 / 2].BackColor = Color.White;
+            gameBoard[32 / 2, 32 / 2] = 1;
+            tail[1].xPos = 32 / 2;
+            tail[1].yPos = 32 / 2;
+            tail[1].isTurningPoint = false;
+
+            timerGameSpeed.Start();
+        }
+
+        private void timerGameSpeed_Tick(object sender, EventArgs e)
+        {
+            if(direction != 0)
+            {
+                if(direction == 1) // sus
+                {
+
+                }
+                else
+                    if(direction == 2) // dreapta
+                    {
+
+                    }
+                    else
+                    {
+                        if(direction == 3) // jos
+                        {
+
+                        }
+                        else
+                        {
+                            if(direction == 4) // stanga
+                            {
+
+                            }
+                        }
+                    }
+            }
         }
     }
 }
