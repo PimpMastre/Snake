a5f5e469a321ce4053a1290ff49f13ea9aa3d179
pimpmastre <gavrilas.andrei@hotmail.com>
2026-06-27 02:50:32 +0300

    moooore

--- Snake/Snake/Form1.Designer.cs
+++ Snake/Snake/Form1.Designer.cs
@@ -654,6 +654,7 @@
             //
             // labelStartGame
             //
+            this.labelStartGame = new System.Windows.Forms.Label();
             this.labelStartGame.AutoSize = true;
             this.labelStartGame.BackColor = System.Drawing.Color.Black;
             this.labelStartGame.Font = new System.Drawing.Font("8BIT WONDER", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
